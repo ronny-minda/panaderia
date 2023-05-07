3be6025014852688581d7f3574e33c88d363510e
ronny-minda <ronny.minda.vera@gmail.com>
2023-05-07 17:21:31 -0600

    listo a produccion2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,15 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="favicon.png"></link>
-        <meta
-          name="viewport"
-          content="user-scalable=no, width=device-width, initial-scale=1.0"
-        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin="true"
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300;700&display=swap"
